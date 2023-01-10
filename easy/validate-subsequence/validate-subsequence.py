2f9f4f23be1a221bdf5d21bf360b7c0173ee102b
pataspatek <patrikpatek3@gmail.com>
2023-01-10 20:52:53 +0100

    validate subsequence solved

---
+++ easy/validate-subsequence/validate-subsequence.py
@@ -0,0 +1,19 @@
+def subsequence_check(array, sequence):
+    index = 0
+    
+    for number in array:
+        if number == sequence[index]:
+            index += 1
+
+        if index == len(sequence):
+            return True
+
+    return False
+
+
+def test_subsequence():
+    assert subsequence_check([1, 2, 3, 4], [2, 3]) == True
+    assert subsequence_check([1, 2, 3, 4], [5, 6]) == False
+    assert subsequence_check([1, 2, 3, 4], [1, 2, 3, 4]) == True
+    assert subsequence_check([1, 2, 3, 4], [1]) == True
+    assert subsequence_check([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10]) == True
