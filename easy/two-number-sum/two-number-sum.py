8eb0828f3230acc2a8e2e4d5b63390f5de7bc2bb
pataspatek <patrikpatek3@gmail.com>
2023-01-11 19:48:24 +0100

    two number sum solution

---
+++ easy/two-number-sum/two-number-sum.py
@@ -0,0 +1,38 @@
+# Time complexity: O(n^2)
+# Space complexity: O(1)
+def two_number_sum(num_array, target_sum):
+    for i in num_array:
+        for j in num_array:
+
+            if i + j == target_sum and i != j:
+                return [i, j]
+
+    return []
+
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+def gpt_solution(num_array, target_sum):
+    nums = []
+
+    for number in num_array:
+
+        if target_sum - number in nums:
+            return [number, target_sum - number]
+
+        else:
+            nums.append(number)
+
+    return []
+
+
+# Integers in the returned list can be in reverse order
+def test():
+    assert gpt_solution([3, 5, -4, 8, 11, 1, -1, 6], 10) == [-1, 11] or gpt_solution([3, 5, -4, 8, 11, 1, -1, 6], 10) == [11, -1]
+    assert gpt_solution([4, 2, 3, 5, 1], 8) == [5, 3] or gpt_solution([4, 2, 3, 5, 1], 8) == [3, 5]
+    assert gpt_solution([3, 4, 7, 10], 15) == []
+
+    assert two_number_sum([3, 5, -4, 8, 11, 1, -1, 6], 10) == [-1, 11] or two_number_sum([3, 5, -4, 8, 11, 1, -1, 6], 10) == [11, -1]
+    assert two_number_sum([4, 2, 3, 5, 1], 8) == [5, 3] or two_number_sum([4, 2, 3, 5, 1], 8) == [3, 5]
+    assert two_number_sum([3, 4, 7, 10], 15) == []
+
